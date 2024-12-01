b850017f8d934a71a0698756c3b1ea6cb53010dd
budi-irawan <budi.irawan171717@gmail.com>
2024-12-01 11:41:37 +0700

    api url

--- src/ipBackend.js
+++ src/ipBackend.js
@@ -1,2 +1,3 @@
-// export const ipBackend = "http://localhost:9000"
-export const ipBackend = "http://18.141.101.253:9000"
+// export const ipBackend = "http://localhost:9000" local
+// export const ipBackend = "http://18.141.101.253:9000" AWS
+export const ipBackend = "http://8.219.164.239:9000" 
